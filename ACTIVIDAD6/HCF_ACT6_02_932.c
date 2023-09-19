5865bb92d99f2c0d9140df0990491f141ea6c5bd
Scalaptia <fernando@haroware.com>
2023-09-18 22:24:50 -0700

    Actividad 6 Parte 2

--- ACTIVIDAD6/HCF_ACT6_02_932.c
+++ ACTIVIDAD6/HCF_ACT6_02_932.c
@@ -3,14 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ALUMNOS 40
+#define MAXTURISTAS 10
+#define MAXPESO 700
 
 //*** PROTOTIPOS DE FUNCIONES  ******
-int msges();
-int msgCiclos();
-void menu();
-void prom();
+int msges(void);
+int msgCiclos(void);
+int validar(int ri, int rf);
+void menu(void);
+void prom(void);
+void sumYMed(void);
+void turistas(void);
+void asig(void);
 
 //****  MAIN PRINCIPAL  *********
 int main()
@@ -28,8 +35,10 @@ int msges()
 	system("CLS");
 	printf("   M  E   N   U \n");
 	printf("1.- PROMEDIOS \n");
-	printf("2.-  \n");
-	printf("3.-  \n");
+	printf("2.- TABLAS \n");
+	printf("3.- SUMA Y MEDIA \n");
+	printf("4.- TURISTAS \n");
+	printf("5.- ESTADO ASIGNATURA \n");
 	printf("0.- SALIR  \n");
 	printf("ESCOGE UNA OPCION: ");
 	scanf("%d", &op);
@@ -49,14 +58,37 @@ void menu()
 			break;
 
 		case 2:
-			// ();
+			tablas();
 			break;
 
 		case 3:
-			// ();
+			sumYMed();
+			break;
+
+		case 4:
+			turistas();
+			break;
+
+		case 5:
+			asig();
 			break;
 		}
 	} while (op != 0);
+}
+//*********************
+// Imprimir un mensaje y validar el numero entero que ingrese el usuario este en un rango dado.
+int validar(int ri, int rf)
+{
+	int num;
+	char cadena[10];
+	do
+	{
+		fflush(stdin);
+		gets(cadena);
+		num = atoi(cadena);
+	} while (num < ri || num > rf);
+
+	return num;
 }
 
 //*********************
@@ -76,16 +108,16 @@ void prom(void)
 	for (i = 1, sd = 0; i <= ALUMNOS; i++)
 	{
 		printf("ALUMNO %d\n", i);
-		printf("Ingrese la calificacion de la unidad 1: ");
-		scanf("%d", &u1);
-		printf("Ingrese la calificacion de la unidad 2: ");
-		scanf("%d", &u2);
-		printf("Ingrese la calificacion de la unidad 3: ");
-		scanf("%d", &u3);
-		printf("Ingrese la calificacion de la unidad 4: ");
-		scanf("%d", &u4);
-		printf("Ingrese la calificacion de la unidad 5: ");
-		scanf("%d", &u5);
+		puts("Ingrese la calificacion de la unidad 1: ");
+		u1 = validar(0, 100);
+		puts("Ingrese la calificacion de la unidad 2: ");
+		u2 = validar(0, 100);
+		puts("Ingrese la calificacion de la unidad 3: ");
+		u3 = validar(0, 100);
+		puts("Ingrese la calificacion de la unidad 4: ");
+		u4 = validar(0, 100);
+		puts("Ingrese la calificacion de la unidad 5: ");
+		u5 = validar(0, 100);
 
 		prom = (u1 + u2 + u3 + u4 + u5) / 5.0;
 		printf("\nPROM = %.2f\n", prom);
@@ -95,6 +127,7 @@ void prom(void)
 			printf("SIN DERECHO\n");
 			sd++;
 		}
+
 		system("PAUSE");
 		system("CLS");
 	}
@@ -106,18 +139,148 @@ void prom(void)
 }
 
 //*********************
-// Calcula el promedio de 40 alumnos y escribe cuantos no tienen derecho a examen.
+// Imprime las tablas de multiplicar del 1 al 10.
 // HCF_ACT6_02_02_932
 void tablas(void)
 {
 	//  VARIABLES LOCALES
-	int i;
+	int i, j;
 	system("CLS");
 
 	//	TITULO
 	printf("   TABLAS\n");
 
 	//	PROGRAMA
+	for (i = 1; i <= 10; i++)
+	{
+		system("CLS");
+		printf("TABLA DEL %d\n\n", i);
+		for (j = 1; j <= 10; j++)
+		{
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+		system("PAUSE");
+	}
+}
+//*********************
+// Leer n cantidad de números dentro de un rango dado, desplegar la suma y media.
+// HCF_ACT6_02_03_932
+void sumYMed(void)
+{
+	//  VARIABLES LOCALES
+	int n, ri, rf, num, suma = 0;
+	system("CLS");
+
+	//	TITULO
+	printf("   SUMA Y MEDIA\n");
+
+	// PROGRAMA
+	printf("Ingrese la cantidad de numeros a ingresar: ");
+	scanf("%d", &n);
+	printf("Ingrese el valor minimo del rango: ");
+	scanf("%d", &ri);
+	printf("Ingrese el valor maximo del rango: ");
+	scanf("%d", &rf);
+
+	printf("Ingrese %d numeros:\n", n);
+	for (int i = 1; i <= n; i++)
+	{
+		printf("Numero %d: ", i);
+		num = validar(ri, rf);
+		suma += num;
+	}
+
+	printf("\n");
+	printf("La suma es: %d\n", suma);
+	printf("La media es: %.2f\n", suma / n + 0.0);
+
+	printf("\n");
+	system("PAUSE");
+}
+//*********************
+// Contar turistas que se subiran a un barco y calcular promedio de peso.
+// HCF_ACT6_02_03_932
+void turistas(void)
+{
+	//  VARIABLES LOCALES
+	float peso, pesoTotal = 0.0;
+	int i;
+	system("CLS");
+
+	//	TITULO
+	printf("   TURISTAS\n");
+
+	//	PROGRAMA
+	for (i = 0; i < MAXTURISTAS; i++)
+	{
+		printf("Ingrese el peso del turista %d: ", i + 1);
+		scanf("%f", &peso);
+
+		if ((pesoTotal + peso) > (MAXPESO + MAXPESO * 0.15))
+		{
+			printf("Sobrepeso permitido excedido\n");
+			break;
+		}
+
+		pesoTotal += peso;
+	}
+
+	printf("\n");
+	printf("El promedio de peso de los turistas es: %.2f kg\n", pesoTotal / i);
+
+	if (i == MAXTURISTAS)
+	{
+		printf("Se llego al maximo de turistas (%d)\n", MAXTURISTAS);
+	}
+	else
+	{
+		printf("Se llego al maximo de peso (%d kg)\n", MAXPESO);
+		printf("Abordo %d turista(s)\n", i);
+	}
+
+	printf("\n");
+	system("PAUSE");
+}
+//*********************
+// Determina el estado de un alumno en una asignatura en base a sus calificaciones.
+// HCF_ACT6_02_03_932
+void asig(void)
+{
+	//  VARIABLES LOCALES
+	int p1, p2, p3, i;
+	float prom;
+
+	//	PROGRAMA
+	for (i = 0; i < 5; i++)
+	{
+		system("CLS");
+		printf("   ESTADO ASIGNATURA\n");
+		puts("Ingrese la calificacion del parcial 1: ");
+		p1 = validar(0, 100);
+		puts("Ingrese la calificacion del parcial 2: ");
+		p2 = validar(0, 100);
+		puts("Ingrese la calificacion del parcial 3: ");
+		p3 = validar(0, 100);
+
+		prom = (p1 + p2 + p3) / 3.0;
+
+		if (prom >= 60)
+		{
+			printf("\nAprobado con calificacion de %.2f\n", prom);
+			break;
+		}
+
+		if (i == 2)
+		{
+			printf("\nBaja temporal (Maximo de intentos)\n");
+			break;
+		}
+
+		printf("\nPromedio: %.2f\n", prom);
+		printf("Intentos: %d\n", i + 1);
+		printf("No has aprobado, debes repetir la materia\n\n");
+		system("PAUSE");
+	}
 
 	printf("\n");
 	system("PAUSE");
